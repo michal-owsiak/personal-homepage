a062c8dbd9728854a78ca6e2eab376594e8c8539
Michal Owsiak <micowsiak@gmail.com>
2023-10-02 16:31:00 +0200

    socials id added

--- src/features/Footer/Socials/index.js
+++ src/features/Footer/Socials/index.js
@@ -3,8 +3,9 @@ import { Wrapper } from "./styled";
 
 const Socials = () => (
   <Wrapper>
-    {socials.map(({ social, link, Logo }) => (
+    {socials.map(({ id, social, link, Logo }) => (
       <a
+        key={id}
         href={link}
         title={social}
         target="_blank"
--- src/socialsList.js
+++ src/socialsList.js
@@ -4,11 +4,13 @@ import { StyledLogo } from "./features/Footer/Socials/styled";
 
 export const socials = [
   {
+    id: 1,
     social: "GitHub",
     link: "https://github.com/michal-owsiak",
     Logo: StyledLogo(GitHubLogo),
   },
   {
+    id: 2,
     social: "Facebook",
     link: "https://www.facebook.com/profile.php?id=100079979666771",
     Logo: StyledLogo(FacebookLogo),
