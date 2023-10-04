3063f2b589ad52c69cba2de1cdc6443ac4c32063
Michal Owsiak <micowsiak@gmail.com>
2023-10-04 17:10:27 +0200

    non-breaking spaces added to copies

--- src/copy.js
+++ src/copy.js
@@ -1,7 +1,7 @@
 export const name = "Michał Owsiak";
 
 export const bio =
-  "Frontend Developer with proficiency in React. Skilled in JavaScript, CSS, and modern web technologies. Explore my portfolio to see how I implement these skills in real projects. Currently seeking new job opportunities in the frontend development field. Feel free to reach out!";
+  "Frontend Developer with proficiency in\u00A0React. Skilled in\u00A0JavaScript, CSS, and\u00A0modern web technologies. Explore my portfolio to see how I\u00A0implement these skills in real projects. Currently seeking new job opportunities in\u00A0the\u00A0frontend development field. Feel free to\u00A0reach out!";
 
 export const skillsetList = [
   "HTML",
@@ -44,4 +44,4 @@ export const errorText = "Sorry, failed to load Github projects. You can check t
 export const mail = "micowsiak@gmail.com";
 
 export const footerCaption =
-  "I’m always open to new projects. If you have a website, dashboard, or mobile app in mind and need some help to make your ideas come to life, feel free to contact me!";
+  "I’m always open to new projects. If you have a website, dashboard, or\u00A0mobile app in mind and need some help to make your ideas come to\u00A0life, feel free to contact me!";
